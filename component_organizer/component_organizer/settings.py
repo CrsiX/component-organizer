71291db6484093c3eaeb67ddb3f50e41fd55d42e
gammelalf <31348226+gammelalf@users.noreply.github.com>
2020-11-26 19:37:53 +0100

    Fixed browseable api by removing some explicit settings

--- component_organizer/component_organizer/settings.py
+++ component_organizer/component_organizer/settings.py
@@ -105,16 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-    ],
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
